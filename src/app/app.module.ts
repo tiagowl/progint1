926255c5053f62f392c8bab0804fcad3e1b6d6ab
tiago <winkellandi123@gmail.com>
2019-11-17 19:46:16 -0300

    Criando o componente livros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LivrosComponent } from './livros/livros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LivrosComponent
   ],
   imports: [
     BrowserModule,
